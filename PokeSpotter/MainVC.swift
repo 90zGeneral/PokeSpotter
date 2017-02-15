9d6309878902ca7d9f95abb4eca4e88e021ab7cf
Roydon Jeffrey <brushdam14823@gmail.com>
2017-02-14 21:18:13 -0500

    Wrote code to set up the mapView and get the user's location and added a custom annotation to represent the user's location. Also, update changes to the user's location by tracking the user's movements.

---
+++ PokeSpotter/MainVC.swift
@@ -0,0 +1,99 @@
+//
+//  MainVC.swift
+//  PokeSpotter
+//
+//  Created by Roydon Jeffrey on 2/13/17.
+//  Copyright © 2017 Italyte. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+class MainVC: UIViewController, MKMapViewDelegate, CLLocationManagerDelegate {
+
+    //Outlet
+    @IBOutlet weak var mapView: MKMapView!
+    
+    //LocationManager
+    let locationManager = CLLocationManager()
+    
+    //State of the centering map
+    var mapHasCenteredOnce = false
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        //Connection btw mapView and the VC thru the delegate
+        mapView.delegate = self
+        
+        //Track the user based on their location
+        mapView.userTrackingMode = MKUserTrackingMode.follow
+
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        
+        //Function call everytime the view appears
+        userLocationAuthStatus()
+    }
+    
+    //Get the user's permission to access their location 
+    func userLocationAuthStatus() {
+        
+        if CLLocationManager.authorizationStatus() == .authorizedWhenInUse {
+            mapView.showsUserLocation = true
+            
+        }else {
+            locationManager.requestWhenInUseAuthorization()
+        }
+    }
+    
+    //Call this function when the user decides whether to permit location access or not
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        if status == .authorizedWhenInUse {
+            mapView.showsUserLocation = true
+        }
+    }
+    
+    //Re-center the map to the user's current location after the user zoom or pan on the map
+    func mapCenteredOnUserLocation(location: CLLocation) {
+        let coordinateRegion = MKCoordinateRegionMakeWithDistance(location.coordinate, 2000, 2000)
+        
+        mapView.setRegion(coordinateRegion, animated: true)
+    }
+    
+    //Update on the user's location
+    func mapView(_ mapView: MKMapView, didUpdate userLocation: MKUserLocation) {
+        
+        if let locate = userLocation.location {
+            
+            //If the map has not been centered before, then center it on user's location and change the state of mapHasCenteredOnce
+            if !mapHasCenteredOnce {
+                mapCenteredOnUserLocation(location: locate)
+                mapHasCenteredOnce = true
+            }
+        }
+    }
+    
+    //To set a custom annotion for the user's location
+    func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+        
+        //Annotation Declaration
+        var annotationView: MKAnnotationView!
+        
+        //Check if the annotation is of type MKUserLocation and set its image to my custom image
+        if annotation.isKind(of: MKUserLocation.self) {
+            annotationView = MKAnnotationView(annotation: annotation, reuseIdentifier: "User")
+            annotationView.image = UIImage(named: "ash")
+        }
+        
+        return annotationView
+    }
+    
+    //Tap when you see a pokemon
+    @IBAction func pokemonSpotted(_ sender: Any) {
+        
+    }
+
+}
+
