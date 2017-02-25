1e344f5730e101efe20fe02b918938b755510943
Roydon Jeffrey <brushdam14823@gmail.com>
2017-02-24 21:41:47 -0500

    A UIPresentationController was added as a controller to manage the facts about the pokemons.

---
+++ PokeSpotter/PokemonFactsPC.swift
@@ -0,0 +1,13 @@
+//
+//  PokemonFactsPC.swift
+//  PokeSpotter
+//
+//  Created by Roydon Jeffrey on 2/24/17.
+//  Copyright © 2017 Italyte. All rights reserved.
+//
+
+import UIKit
+
+class PokemonFactsPC: UIPresentationController {
+
+}
